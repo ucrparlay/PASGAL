35da69ba0b534b38758fe443a12ddb4c68f240bd
Nishanth <nishanth230499@gmail.com>
2025-02-05 15:15:46 -0800

    added scripts for basic statistics

--- src/basic_statistics/basic_statistics.cpp
+++ src/basic_statistics/basic_statistics.cpp
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
     
   } else {
     fprintf(stdout, "Running on %s: |V|=%zu, |E|=%zu, density=%f, degree=%f\n",
-            input_path, G.n, G.m, (double)G.m, (double)G.n/(double)G.m);
+            input_path, G.n, G.m, ((double)G.m * 2) / ((double)G.n * ((double)G.n - 1)), (double)G.n/(double)G.m);
   }
   return 0;
 }
