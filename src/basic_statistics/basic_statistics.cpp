f1d98729a8eb2ad646fd4448cc08815b29ce8f80
Nishanth Prasanna Kumar <146760605+nishanth230499@users.noreply.github.com>
2025-02-08 13:14:06 -0800

    Update basic_statistics.cpp(degree to avg_degree)

--- src/basic_statistics/basic_statistics.cpp
+++ src/basic_statistics/basic_statistics.cpp
@@ -33,10 +33,10 @@ int main(int argc, char *argv[]) {
   Graph G;
   G.read_graph(input_path);
   if(return_json) {
-    fprintf(stdout, "{\"vertices_count\": %zu, \"edges_count\": %zu, \"density\": %f, \"degree\": %f}\n",
+    fprintf(stdout, "{\"vertices_count\": %zu, \"edges_count\": %zu, \"density\": %f, \"avg_degree\": %f}\n",
             G.n, G.m, ((double)G.m * 2) / ((double)G.n * ((double)G.n - 1)), (double)G.n/(double)G.m);
   } else {
-    fprintf(stdout, "Running on %s: |V|=%zu, |E|=%zu, density=%f, degree=%f\n",
+    fprintf(stdout, "Running on %s: |V|=%zu, |E|=%zu, density=%f, avg_degree=%f\n",
             input_path, G.n, G.m, ((double)G.m * 2) / ((double)G.n * ((double)G.n - 1)), (double)G.n/(double)G.m);
   }
   return 0;
