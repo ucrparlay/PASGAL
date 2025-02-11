c7a53cb9f673b5da6811dc2a23d82a774af441ff
Nishanth <nishanth230499@gmail.com>
2025-02-11 11:50:10 -0800

    added code to compute max min in out degree

--- src/basic_statistics/basic_statistics.cpp
+++ src/basic_statistics/basic_statistics.cpp
@@ -8,7 +8,7 @@
 int main(int argc, char *argv[]) {
   if (argc == 1) {
     fprintf(stderr,
-            "Usage: %s [-i input_file] [-s] [-v]\n"
+            "Usage: %s [-i input_file] [-j]\n"
             "Options:\n"
             "\t-i,\tinput file path\n"
             "\t-j,\tprint in JSON format\n",
@@ -17,7 +17,6 @@ int main(int argc, char *argv[]) {
   }
   char c;
   bool return_json = false;
-  bool symmetrized = false;
   char const *input_path = nullptr;
   while ((c = getopt(argc, argv, "i:j")) != -1) {
     switch (c) {
@@ -27,45 +26,50 @@ int main(int argc, char *argv[]) {
       case 'j':
         return_json = true;
         break;
-      case 's':
-        symmetrized = true;
-        break;
     }
   }
-
-
 
   printf("Reading graph...\n");
   Graph G;
-
   G.read_graph(input_path);
-  G.symmetrized = symmetrized;
-  // if (!G.symmetrized) {
-  G.make_inverse();
-  // }
 
-  bool match = true;
+  G.make_inverse();
+
+  size_t max_out_degree = 0, min_out_degree = -1, zero_out_degree_count = 0;
+  size_t max_in_degree = 0, min_in_degree = -1, zero_in_degree_count = 0;
+  size_t out_degree, in_degree;
+
   for(size_t i = 0; i < G.n; i++) {
-    if(G.offsets[i] != G.in_offsets[i]) {
-      match = false;
+    out_degree = G.offsets[i+1] - G.offsets[i];
+    in_degree = G.in_offsets[i+1] - G.in_offsets[i];
+
+    if(out_degree > max_out_degree) {
+      max_out_degree = out_degree;
+    }
+    if(min_out_degree == -1 || out_degree < min_out_degree) {
+      min_out_degree = out_degree
+    }
+    if(out_degree == 0) {
+      zero_out_degree_count++;
+    }
+
+    if(in_degree > max_in_degree) {
+      max_in_degree = in_degree;
+    }
+    if(min_in_degree == -1 || in_degree < min_in_degree) {
+      min_in_degree = in_degree
+    }
+    if(in_degree == 0) {
+      zero_in_degree_count++;
     }
   }
-  for(size_t i = 0; i < G.m; i++) {
-    if(G.edges[i] != G.in_edges[i]) {
-      match = false;
-    }
-  }
-  if(match) {
-    fprintf(stdout, "The graph matched the symmetrized matched.");
-  } else {
-    fprintf(stdout, "The graph did not matched the symmetrized matched.");
-  }
+
   if(return_json) {
-    fprintf(stdout, "{\"vertices_count\": %zu, \"edges_count\": %zu, \"density\": %f, \"avg_degree\": %f}\n",
-            G.n, G.m, ((double)G.m * 2) / ((double)G.n * ((double)G.n - 1)), (double)G.m/(double)G.n);
+    fprintf(stdout, "{\"vertices_count\": %zu, \"edges_count\": %zu, \"max_out_degree\": %zu, \"min_out_degree\": %zu, \"zero_out_degree_count\": %zu, \"max_in_degree\": %zu, \"min_in_degree\": %zu, \"zero_in_degree_count\": %zu}\n",
+            G.n, G.m, max_out_degree, min_out_degree, zero_out_degree_count, max_in_degree, min_in_degree, zero_in_degree_count);
   } else {
-    fprintf(stdout, "Running on %s: |V|=%zu, |E|=%zu, density=%f, avg_degree=%f\n",
-            input_path, G.n, G.m, ((double)G.m * 2) / ((double)G.n * ((double)G.n - 1)), (double)G.m/(double)G.n);
+    fprintf(stdout, "Running on %s:\n|V| = %zu\n|E| = %zu\nMax Out-degree = %zu\nMin Out-degree = %zu\nZero Out-degree Count = %zu\nMax In-degree = %zu\nMin In-degree = %zu\nZero In-degree Count = %zu\n",
+            G.n, G.m, max_out_degree, min_out_degree, zero_out_degree_count, max_in_degree, min_in_degree, zero_in_degree_count);
   }
   return 0;
 }
